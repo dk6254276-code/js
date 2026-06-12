56a51720273c6306f6d578ed0c195ecff023c2aa
dk6254276-code <dk6254276@gmail.com>
2026-06-12 11:20:34 +0000

    Add initial variable and datatype examples; update test.js logging format

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,18 @@
+ const accountId = 13346
+ let emailId = "dk6254276@gmail.com"
+ var accountPassword = "12345"
+  accountCity = "Patna"
+
+ // accountId = 4376 not allowed
+ emailId = "dk99@gmail.com"
+ accountCity="Agartala"
+ accountPassword = " 00000"
+ let AccountState
+
+ console.log(accountId)
+
+ /*
+ not to use var
+ bcs  of issue in block scope and functional scope
+ */
+  console.table([accountCity, emailId, accountPassword, AccountState])
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // we are using nodejs not browser
+
+console.log("Deepak")
+
+let name = " Deepak";
+let age = 23
+let isLoggedIn = true
+
+// number = 2 to powe 53
+// bigint( too large num)
+
+// string => " "
+// boolean => true/false
+//num => standaloneVAlue(null)
+//undefined => 
+    // symbol => unique ness
+
+console.log(typeof age)
+console.log(typeof undefined) // undefined
+console.log(typeof null) //object
