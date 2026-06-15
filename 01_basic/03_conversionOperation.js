09b8b9f4b3f6fbf148a55b5c1f0933c5a675013f
dk6254276-code <dk6254276@gmail.com>
2026-06-15 06:36:40 +0000

    conversiion of number

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = 33
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueinNumber = Number(score)
+console.log(typeof score)
+console.log(score)
+
+//"33" => 33
+// "33abc" => NaN
+// true/false =>  1/0
+// null => 0
+//undefine => NaN
+
+let isLoggedIn= 1
+let  booleanIsLoogenIn = Boolean(isLoggedIn)
+console.log(booleanIsLoogenIn)
+
+//"" => false; // "deepak"=> true;
+
+let someNum = 33
+
+let stringNum = String(someNum)
+console.log(stringNum)
+console.log(typeof stringNum)
