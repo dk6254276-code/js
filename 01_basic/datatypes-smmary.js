eb470fca8b9eae34682ba2d3f91b06def010e9bc
dk6254276-code <dk6254276@gmail.com>
2026-06-16 07:19:00 +0000

    datatypes

--- 01_basic/datatypes-smmary.js
+++ 01_basic/datatypes-smmary.js
@@ -32,13 +32,36 @@ const myfunction = function() {    // typeof: function
     console.log("Hello world");
 }
 
-console.log(typeof score)
-console.log(typeof scoreVal)
-console.log(typeof isLoggedIn)
-console.log(typeof outSidetemp)
-console.log(typeof userEmail)
-console.log(typeof id)
-console.log(typeof bigNum)
-console.log(typeof heros)
-console.log(typeof myObj)
-console.log(typeof myFunction)
+// console.log(typeof score)
+// console.log(typeof scoreVal)
+// console.log(typeof isLoggedIn)
+// console.log(typeof outSidetemp)
+// console.log(typeof userEmail)
+// console.log(typeof id)
+// console.log(typeof bigNum)
+// console.log(typeof heros)
+// console.log(typeof myObj)
+// console.log(typeof myFunction)
+
+//****************** Memory**********
+
+// stack (premitive) , Heap (Non-premitive)
+
+let myYoutubeCh = "hiteshchaudhry"
+
+let another = myYoutubeCh
+another = "chaiorcode"
+//  console.log(myYoutubeCh)
+//   console.log(another)
+
+let myObj5 = {
+    email : "dk625427@gamil.com",
+    upi : "myrag@ibl"
+}
+
+let myObj2= myObj5
+
+myObj2.email = "dk6254276@gamil.com"
+
+console.log(myObj5.email)
+console.log(myObj2.email)
